00312031e31754f929f0810e583cfe2122ceffb3
Grigory Fateyev <gfborn@gmail.com>
2017-02-15 18:16:55 +0300

    Add next step.

--- sc_customizer/js/app.js
+++ sc_customizer/js/app.js
@@ -6,15 +6,19 @@ var main = {
     3: 'rect:1x1',
     4: 'rect:free',
     5: 'ellipse:16x9',
-    6: 'circle:free'
+    6: 'circle:free',
+    7: 'trapeze:free'
   }
   , colors: ['red', 'lime', 'blue']
   , slots: {}
 
   , stage: null
   , layer: null
+  , slot_name: null
 
   , makeShape: function(name, shape, dim) {
+    var self = this;
+
     // проверяем если есть фигура с именем name и удаляем её
     var figures = this.stage.find('.' + name);
     if (figures.length) {
@@ -37,37 +41,49 @@ var main = {
     }
 
     // создаём фигуру
-    var figure;
+    var figure
     if (shape == 'rect') {
       figure = new Konva.Rect({
         width: width,
         height: height,
-        prop: dim,
-        name: name
+        prop: dim
       });
     } else if (shape == 'circle') {
       figure = new Konva.Circle({
-        radius: (width + height) / 3,
-        name: name
-      });    
+        radius: (width + height) / 3
+      });
+    } else if (shape == 'trapeze') {
+      figure = new Konva.Shape({
+        sceneFunc: function(context) {
+          context.beginPath();
+          context.moveTo(50, 70);
+          context.lineTo(690, 50);
+          context.lineTo(690, 430);
+          context.lineTo(50, 410);
+          context.lineTo(50, 70);
+          context.closePath();
+          context.fillStrokeShape(this);
+        }
+      });
     } else {
       var x = (width + height) / 3;
       figure = new Konva.Ellipse({
         radius: {
           x: x,
           y: x / 16 * 9
-        },
-        name: name
+        }
       });
     }
     var attrs = {
+      name: name,
       fill: color,
       stroke: 'white',
       strokeWidth: 2,
       shadowColor: 'black',
       shadowBlur: 10,
-      shadowOffset: {x : 5, y : 5},
-      shadowOpacity: 0.5      
+      shadowOffsetX: 5,
+      shadowOffsetY: 5,
+      shadowOpacity: 0.5,
     }
     figure.setAttrs(attrs);
 
@@ -92,26 +108,92 @@ var main = {
       this.addAnchor(group, figure.getAttr('width'), figure.getAttr('height'), 'bottomRight', name);
     } else if (shape == 'circle') {
       this.addAnchor(group, figure.radius(), 0, 'rightMiddle', name);
+    } else if (shape == 'trapeze') {
+      //this.addAnchor(group, 690, 430, 'bottomRightT', name);
     } else {
       this.addAnchor(group, figure.radiusX(), 0, 'rightMiddleE', name);
       this.addAnchor(group, 0, figure.radiusY(), 'bottomMiddle', name);
     }
-
+    
     // добавить слой в stage
     this.stage.add(this.layer);
 
     this.slots[name] = {
       slot: figure,
-      color: figure.getFill()
+      color: figure.getFill(),
+      strokeWidth: figure.getAttr('strokeWidth'),
+      shadowBlur: figure.getAttr('shadowBlur'),
+      shadowOffsetX: figure.getAttr('shadowOffsetX'),
+      shadowOffsetY: figure.getAttr('shadowOffsetY'),
+      shadowOpacity: figure.getAttr('shadowOpacity'),
+      shadowColor: figure.getAttr('shadowColor'),
+      stroke: figure.getAttr('stroke')
     };
+
+    // добавляем картинку заполнитель
+    this.fillPattern(figure);
   }
 
+  , fillPattern: function(figure) {
+    var self = this;
+    var name = figure.getName();
+    var imageNumber = name.match(/\d+/)[0];
+    var shape = figure.getClassName();
+    
+    var w = figure.width();
+    var h = figure.height();
+    var imageScale = 0;
+    if (shape == 'Shape') {
+      imageScale = 410 * 100 / 720 / 100;
+    } else if (w < h && figure.prop != 'free') {
+      imageScale = 100 / 1280 * w / 100;
+    } else {
+      imageScale = 100 / 720 * h / 100;
+    }
+
+    // добавляем картинку заполнитель
+    var imageObj = new Image();
+    imageObj.onload = function() {
+      figure.fillPatternImage(imageObj);
+      figure.fillPatternScale({x: imageScale, y: imageScale});
+      figure.fillPriority('pattern');
+      if (shape == 'Ellipse' || shape == 'Circle') {
+        figure.fillPatternOffset({x: 1280 / 2, y: 720 / 2})
+      } else if (shape != 'Shape') {
+        figure.fillPatternOffsetX((1280 - 720 / h * w) / 2)
+      }
+      self.layer.draw();
+    }
+    imageObj.crossOrigin = 'anonymous';
+    imagePath = 'https://dl.dropboxusercontent.com/u/65129102/E' + imageNumber + '.png';
+    //imagePath = 'img/E' + imageNumber + '.png';
+    imageObj.src = imagePath;
+    
+    self.slots[name].pattern = imageObj;
+  }
+
+  , setupOptions: function(key, value) {
+    try {
+      var name = this.slot_name;
+      var slot = this.slots[name].slot;
+      slot.setAttr(key, value);
+      this.stage.draw();
+      this.slots[name][key] = value;
+    } catch (err) {
+      alert('Сначала создайте фигуру!')
+    }
+  }
+  
   , setToTop: function(name) {
     var figures = this.stage.find('.' + name);
     if (figures.length) {
       var figure = figures[0];
       var group = figure.getParent();
-      group.moveToTop();
+      if (group.getClassName() == 'Layer') {
+        figure.moveToTop();
+      } else {
+        group.moveToTop();
+      }
       this.layer.draw();
     }
   }
@@ -161,6 +243,8 @@ var main = {
       var bottomRight = group.get('.bottomRight')[0];
     } else if (figureName == 'Circle') {
       var rightMiddle = group.get('.rightMiddle')[0];
+    } else if (figureName == 'Shape') {
+      var bottomRightT = group.get('.bottomRightT')[0];
     } else {
       var rightMiddleE = group.get('.rightMiddleE')[0];
       var bottomMiddle = group.get('.bottomMiddle')[0];
@@ -206,6 +290,8 @@ var main = {
         bottomMiddle.setX(0);
         rightMiddleE.setX(anchorY / 9 * 16);
         break;
+      case 'bottomRightT':
+        break;
     }
 
     if (figureName == 'Rect') {
@@ -223,7 +309,7 @@ var main = {
       }
     } else if (figureName == 'Circle') {
       figure.radius(anchorX);
-    } else {
+    } else if (figureName == 'Ellipse') {
       if (activeAnchor.getName() == 'bottomMiddle') {
         figure.setRadiusY(anchorY);
         figure.setRadiusX(anchorY / 9 * 16);
@@ -231,13 +317,14 @@ var main = {
         figure.setRadiusX(anchorX);
         figure.setRadiusY(anchorX / 16 * 9);
       }
+    } else {
+      this.drawTrapeze(figure, anchorX, anchorY);
     }
+    
+    this.fillPattern(figure);
   }
 
   , addAnchor: function(group, x, y, name, fname, hide) {
-    var stage = group.getStage();
-    var layer = group.getLayer();
-
     var anchor = new Konva.Circle({
       x: x,
       y: y,
@@ -272,13 +359,13 @@ var main = {
       var layer = this.getLayer();
       document.body.style.cursor = 'pointer';
       this.setStrokeWidth(4);
-      layer.draw();
+      self.layer.draw();
     });
     anchor.on('mouseout', function() {
       var layer = this.getLayer();
       document.body.style.cursor = 'default';
       this.setStrokeWidth(2);
-      layer.draw();
+      self.layer.draw();
     });
 
     group.add(anchor);
@@ -291,6 +378,7 @@ var main = {
       return
     }
 
+    var self = this;
     var stage = this.stage;
     var layer = this.layer;
 
@@ -298,71 +386,110 @@ var main = {
     var ctx = c.getContext('2d');
     
     this.actAnchors('hide');
-
     stage.draw();
     
-    var zip = JSZip();    
-    var bkg = layer.get('Image')[0];
+    //var zip = JSZip();    
+    var bkg = layer.get('.background')[0];
     bkg.visible(true);
     stage.draw();
     var imageData1 = stage.toDataURL({pixelRatio: 1});
-    zip.file('scene_example.png', imageData1.substr(imageData1.indexOf(',') + 1), {base64: true});
+    //zip.file('scene_example.png', imageData1.substr(imageData1.indexOf(',') + 1), {base64: true});
     bkg.visible(false);
 
     // чистим фигуры
     for (var slot_name in this.slots) {
       var slot = this.slots[slot_name].slot;
-      //slot.strokeEnabled(false);
       slot.shadowEnabled(false);
+      slot.fillPriority('color');
+    }
+    var frames = layer.get('.frame1');
+    if (frames.length) {
+      frames[0].hide();
     }
     stage.draw();
+
     var imageData2 = stage.toDataURL({pixelRatio: 1});
-    zip.file('scene_slots.png', imageData2.substr(imageData2.indexOf(',') + 1), {base64: true});
+    //zip.file('scene_slots.png', imageData2.substr(imageData2.indexOf(',') + 1), {base64: true});
     var imageSlotsData = ctx.getImageData(0, 0, stage.getWidth(), stage.getHeight());
 
     for (var slot_name in this.slots) {
       var slot = this.slots[slot_name].slot;
-      //slot.strokeEnabled(true);
       slot.setFill(null);
+      slot.fillPatternImage(null);
       slot.shadowEnabled(true);
     }
     stage.draw()
 
-    // холст только с рамками и тенями от них
-    var imageStrokesData = ctx.getImageData(0, 0, stage.getWidth(), stage.getHeight());
-    
-    // прячем скрытые рамки и тени
-    var data = imageSlotsData.data;
-    for(var i=0; i<data.length; i+=4) {
-      if ((data[i] == 255 && data[i+1] == 0 && data[i+2] == 0)
-          || (data[i] == 0 && data[i+1] == 255 && data[i+2] == 0)
-          || (data[i] == 0 && data[i+1] == 0 && data[i+2] == 255)) {
-        // Основная работа
-        imageStrokesData.data[i+3] = 0;
+    var frames = layer.get('.frame1');
+    if (!frames.length) {
+      // холст только с рамками и тенями от них
+      var imageStrokesData = ctx.getImageData(0, 0, stage.getWidth(), stage.getHeight());
+      
+      // прячем скрытые рамки и тени
+      var data = imageSlotsData.data;
+      for(var i=0; i<data.length; i+=4) {
+        if ((data[i] == 255 && data[i+1] == 0 && data[i+2] == 0)
+            || (data[i] == 0 && data[i+1] == 255 && data[i+2] == 0)
+            || (data[i] == 0 && data[i+1] == 0 && data[i+2] == 255)) {
+          // Основная работа
+          imageStrokesData.data[i+3] = 0;
+        }
       }
+
+      this.data = imageStrokesData;
+      ctx.putImageData(imageStrokesData, 0, 0);
+    } else {
+      frames[0].show();
+      stage.draw();
     }
 
-    this.data = imageStrokesData;
-    ctx.putImageData(imageStrokesData, 0, 0);
-    
     var imageData3 = stage.toDataURL({pixelRatio: 1});
-    zip.file('scene.png', imageData3.substr(imageData3.indexOf(',') + 1), {base64: true});
+    //zip.file('scene.png', imageData3.substr(imageData3.indexOf(',') + 1), {base64: true});
 
-    var self = this;
-    zip.generateAsync({type: 'blob'}).then(function(content) {
-      saveAs(content, name + '.zip');
+    /* zip.generateAsync({type: 'blob'}).then(function(content) {
+     *   saveAs(content, 'sc_' + name + '.zip');
 
-      // возвращаем фигуры
-      for (var slot_name in self.slots) {
-        var slot = self.slots[slot_name].slot;
-        slot.show();
-        slot.strokeEnabled(true);
-        slot.shadowEnabled(true);
-        slot.setFill(self.slots[slot_name].color);
-        self.layer.draw();
-      }
-      self.actAnchors('show');
-    });
+     *   // возвращаем фигуры
+     *   self.returnFigures();
+     * });*/
+
+    var data = {
+      scene_example: imageData1,
+      scene_slots: imageData2,
+      scene: imageData3
+    };
+    var json = JSON.stringify(data);
+    var blob = new Blob([json], {type: "application/json"});
+    var url  = URL.createObjectURL(blob);
+
+    var link = document.createElement('a');
+    link.download = "sc_" + name + ".json";
+    link.href = url;
+    link.click();
+
+    this.returnFigures();
+  }
+
+  , returnFigures: function() {
+    for (var slot_name in this.slots) {
+      var slot = this.slots[slot_name].slot;
+      slot.show();
+      slot.strokeEnabled(true);
+      slot.shadowEnabled(true);
+      slot.setAttrs({
+        fill: this.slots[slot_name].color,
+        stroke: this.slots[slot_name].stroke,
+        strokeWidth: this.slots[slot_name].strokeWidth,
+        shadowColor: this.slots[slot_name].shadowColor,
+        shadowBlur: this.slots[slot_name].shadowBlur,
+        shadowOffsetX: this.slots[slot_name].shadowOffsetX,
+        shadowOffsetY: this.slots[slot_name].shadowOffsetY,
+        shadowOpacity: this.slots[slot_name].shadowOpacity
+      });
+      slot.fillPatternImage(this.slots[slot_name].pattern);
+      slot.fillPriority('pattern');
+    }
+    this.actAnchors('show');
   }
 
   , init: function() {
@@ -394,22 +521,40 @@ var main = {
       });
     });
 
+    // открываем модальное окно и меняем тайтл
+    $('#optionsModal').on('show.bs.modal', function(event) {
+      var button = $(event.relatedTarget)
+      var slot_name = button.data('slotname')
+      self.slot_name = slot_name
+      var modal = $(this)
+      modal.find('.modal-title').text('Опции слота ' + slot_name)
+    })
+    
     // событие показать результат
     document.querySelector('#show').addEventListener('click', function() {
-      self.actAnchors('hide');
-      var bkg = self.layer.get('Image')[0];
+      var bkg = self.layer.get('.background')[0];
       bkg.visible(true);
-      self.stage.draw();
+      for (var slot_name in self.slots) {
+        var slot = self.slots[slot_name].slot;
+        slot.fillPriority('color');
+      }
+      self.actAnchors('hide');
 
       var imageData = self.stage.toDataURL({pixelRatio: 1});
       var image = new Image();
+      //image.onload = function() {}
       image.src = imageData;
 
       var win = window.open('', '_blank');
       win.document.write(image.outerHTML);
-
+      
       bkg.visible(false);
-      self.actAnchors('show');
+      /*for (var slot_name in self.slots) {
+        var slot = self.slots[slot_name].slot;
+        slot.fillPriority('pattern');
+      }
+      self.actAnchors('show');*/
+      self.returnFigures();
     });
 
     var imageObj = new Image();
@@ -420,7 +565,8 @@ var main = {
         image: imageObj,
         width: 1280,
         height: 720,
-        visible: false
+        visible: false,
+        name: 'background'
       });
 
       self.layer.add(bkgrnd);
@@ -435,6 +581,28 @@ var main = {
 
     // событие скачать архив
     document.querySelector('#download').addEventListener('click', this.download.bind(this));
+
+    // событе грузим рамку
+    document.querySelector('#input-file').addEventListener('change', function() {
+      var file = this.files[0];
+
+      var img = new Image();
+      img.onload = function() {
+        var frame = new Konva.Image({
+          x: 0,
+          y: 0,
+          image: img,
+          width: 1280,
+          height: 720,
+          draggable: true,
+          name: 'frame1'
+        });
+
+        self.layer.add(frame);
+        self.stage.draw();
+      }
+      img.src = URL.createObjectURL(file);
+    });
   }
 }
 
